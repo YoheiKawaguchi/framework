3ebb1b1199ed9a2dc22a7211aa91158e0eeb9496
Yohei <yoheikawaguchi@gmail.com>
2013-05-09 16:03:15 +1000

    Modified Copyright note

--- module/Common/View/Layout/Footer.php
+++ module/Common/View/Layout/Footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
     <p class="footer_navi"><a href="#">About Us</a>&nbsp;|&nbsp;<a href="#">Privacy Policy</a>&nbsp;|&nbsp;<a href="#">Terms &amp; Conditions</a></p>
-    <p class="copyright">Copyright &copy; 2013 Yohei Kawaguchi - All rights reserved</p>
+    <p class="copyright">Copyright &copy; 2013 *your copyright here* - All rights reserved</p>
 </div>
 
 </body>
